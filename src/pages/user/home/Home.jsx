634501efbd4bb09b4b8d332c4d6f021490d2bac0
Shibily-kms <kmsshibily@gmail.com>
2023-04-10 19:16:58 +0530

    landing component completed

--- src/pages/user/home/Home.jsx
+++ src/pages/user/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../../../components/user/header/Header";
+import Landing from "../../../components/user/landing/Landing";
 
 function Home() {
   return (
     <div className="home-page">
       <div className="header-div">
         <Header />
+      </div>
+      <div className="landing-div">
+        <Landing />
       </div>
     </div>
   );
